53f1496212c50e3228bd1e996cbf7e079b168211
harshit-dev-ui <65815998+harshit-dev-ui@users.noreply.github.com>
2024-06-21 01:47:45 +0530

    set up routes and auth-layout

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Loader } from "react-js-loader";
+function Protected({ children, authentication = true }) {
+  const authStatus = useSelector((state) => state.auth.status);
 
-function Protected() {
-  return <div>Protected</div>;
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, authentication, navigate]);
+
+  return loader ? <Loader /> : <>{children}</>;
 }
 
 export default Protected;
+
+// if(true){
+//   if (false) {
+//     navigator("/login")
+//   }
+// }
